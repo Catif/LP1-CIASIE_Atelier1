c3bca5cf1c1ccd885c0d08a5cf5136be9f9e0d5a
Bradley BARBIER <bradley.barbier@outlook.fr>
2022-11-09 14:37:33 +0100

    Navbar updated if user connected

--- src/classes/view/AppView.php
+++ src/classes/view/AppView.php
@@ -2,6 +2,9 @@
 
 namespace atelier\view;
 
+use atelier\auth\Authentification;
+use Illuminate\Contracts\Auth\Authenticatable;
+
 abstract class AppView extends AbstractView implements Renderer{
     /* Méthode makeBody 
      * 
@@ -36,49 +39,62 @@ abstract class AppView extends AbstractView implements Renderer{
     }
 
     protected function makeNavbar(): string {
-        return <<<BLADE
-            <nav>
-                <h3>PhotoMedia</h3>
-                
-                <div class="top">
-                    <form action="" method="GET" class="search">
-                        <select name="search" id="search">
-                            <option value="picture" selected>Image</option>
-                            <option value="galery">Galerie</option>
-                        </select>
-                        <div class="search">
-                            <input type="text" name="q" placeholder="Rechercher">
-                            <button type="submit"><i class="bi bi-search"></i></button>
-                        </div>
-                    </form>
+        $navItems = "";
+        if (Authentification::connectedUser()) {
+            $urlProfile = $this->router->urlFor('profile');
+            
+            $navItems = <<<BLADE
+            <a href="${urlProfile}"><h2>Profil</h2></a>
+            <a href=""><h2>A propos</h2></a>
+            <a href=""><h2>Déconnexion</h2></a>
+            <a href="" class="mediaquery"><h2>Profil</h2></a>
+            <div id="dropdownNav">
+                <a href=""><img src="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"></a>
 
-                    <i class="bi bi-plus-lg"></i>
-                </div>
-
-                <i class="bi bi-list"></i>
-
-                <div class="list-items">
-                    <a href=""><h2>Accueil</h2></a>
-                    <!-- <a href=""><h2>Profil</h2></a> -->
-                    <a href=""><h2>A propos</h2></a>
-                    <!-- <a href=""><h2 id="login">Connexion</h2></a> -->
-                    <!-- <a href=""><h2 id="register">Inscription</h2></a> -->
-                    <!-- <a href=""><h2>Déconnexion</h2></a> -->
-                    <a href="" class="mediaquery"><h2>Profil</h2></a>
-                    <div id="dropdownNav">
-                        <a href=""><img src="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"></a>
-
-                        <div class="dropdown-content">
-                            <div class="items">
-                                <a href="">Profil</a>
-                                <a href="">Paramètres</a>
-                                <a href="">Déconnexion</a>
-                            </div>
-                        </div>
+                <div class="dropdown-content">
+                    <div class="items">
+                        <a href="">Profil</a>
+                        <a href="">Paramètres</a>
+                        <a href="">Déconnexion</a>
                     </div>
                 </div>
+            </div>
+            BLADE;
+        } else {
+            $navItems = <<<BLADE
+            <a href=""><h2>A propos</h2></a>
+            <a href=""><h2 id="login">Connexion</h2></a>
+            <a href=""><h2 id="register">Inscription</h2></a>
+            BLADE;
+        }
 
-            </nav>
+        return <<<BLADE
+        <nav>
+            <h3>PhotoMedia</h3>
+            
+            <div class="top">
+                <form action="" method="GET" class="search">
+                    <select name="search" id="search">
+                        <option value="picture" selected>Image</option>
+                        <option value="galery">Galerie</option>
+                    </select>
+                    <div class="search">
+                        <input type="text" name="q" placeholder="Rechercher">
+                        <button type="submit"><i class="bi bi-search"></i></button>
+                    </div>
+                </form>
+
+                <i class="bi bi-plus-lg"></i>
+            </div>
+
+            <i class="bi bi-list"></i>
+
+            <div class="list-items">
+                <a href=""><h2>Accueil</h2></a>
+                ${navItems}
+            </div>
+
+        </nav>
         BLADE;
     }
 }
