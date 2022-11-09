1bbf8dbb2ee7687bffefbf04c980062090a1e32f
Christopher JUE <christopher.jue@outlook.com>
2022-11-09 11:29:57 +0100

    Commit before a develop pull: fixing router bug

--- src/classes/view/ProfileView.php
+++ src/classes/view/ProfileView.php
@@ -6,6 +6,9 @@ class ProfileView extends AppView {
     public function render() : string{
         $html = <<<EOF
             <h2>Mes galeries</h2>
+            <div>
+                
+            </div>
         EOF;
 
         return $html;
