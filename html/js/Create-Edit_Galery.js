3631877d9c207a9d6c5bf96da182ff500ef4b14f
Bradley BARBIER <bradley.barbier@outlook.fr>
2022-11-10 11:48:50 +0100

    Create Modal - Mobile version

--- html/js/Create-Edit_Galery.js
+++ html/js/Create-Edit_Galery.js
@@ -1,5 +1,11 @@
+let modal, imagesListEl, tabFile = [];
 
 function init(){
+    imagesListEl = document.querySelector('.images-list')
+    modal = document.querySelector('.modal')
+    console.log(modal)
+    initModal();
+
     document.querySelector('#imagesUpload').addEventListener('change', (e) => {
         let files = e.target.files
         addImages(files)
@@ -7,7 +13,33 @@ function init(){
 }
 
 
+function initModal(){
+    modal.querySelector('.footer [dataModal="valider"]')
+        .addEventListener('click', (e) => {
+            modal.classList.remove('active')
+            let rang = modal.querySelector('#numberImage').innerText
 
+            let newTitlePicture = modal.querySelector('.body input[name="MODAL-title-image"]').value
+            let newTagPicture = modal.querySelector('.body input[name="MODAL-tag-image"]').value
+
+            let divImage = imagesListEl.querySelector('#file-' + rang)
+            
+            divImage.querySelector('p').innerText = newTitlePicture
+            divImage.querySelector('input[name="title-image-' + rang + '"]').value = (newTitlePicture== undefined) ? tabFile[rang].name : newTitlePicture;
+            divImage.querySelector('input[name="tag-image-' + rang + '"]').value = (newTagPicture == undefined) ? '' : newTagPicture; 
+
+            tabFile[rang].tag = newTagPicture
+            
+            console.log('valider :')
+            console.log(tabFile)
+    })
+
+    modal.querySelector('.footer [dataModal="annuler"]')
+        .addEventListener('click', (e) => {
+            modal.classList.remove('active')
+            console.log('valider')
+    })
+}
 
 
 
@@ -15,8 +47,7 @@ function init(){
 // Gestion d'ajout d'images
 function addImage(image){
     console.log(image)
-    let imagesListEl = document.querySelector('.images-list')
-    let rang = imagesListEl.length + 1
+    let rang = image.rang
 
     let div = document.createElement('div')
     div.classList.add('image-file')
@@ -32,12 +63,30 @@ function addImage(image){
         </div>
     `
 
+    div.querySelector('.bi-pencil-square').addEventListener('click', (e) => {
+        modal.classList.add('active')
+        modal.querySelector('#numberImage').innerText = rang
+
+        let imageFile = tabFile[rang]
+
+        modal.querySelector('.body img').src = URL.createObjectURL(imageFile)
+        modal.querySelector('.body input[name="MODAL-title-image"]').value = imageFile.name
+        modal.querySelector('.body input[name="MODAL-tag-image"]').value = imageFile.tag
+    })
+
+    div.querySelector('.bi-x-lg').addEventListener('click', (e) => {
+        imagesListEl.removeChild(div)
+    })
+    
     imagesListEl.appendChild(div)
 }
 
 function addImages(tab){
-    console.log(tab)
     for(let i = 0; i < tab.length; i++){
+        tabFile.push(tab[i])
+        tabFile[tabFile.length-1].title = tab[i].name;
+        tabFile[tabFile.length-1].rang = tabFile.length-1;
+        tabFile[tabFile.length-1].tag = ''
         addImage(tab[i])
     }
 }
--- src/classes/view/CreateGaleryView.php
+++ src/classes/view/CreateGaleryView.php
@@ -7,11 +7,19 @@ class CreateGaleryView extends AppView {
         $urlActionForm = $this->router->urlFor('create-galery');
         $urlRetour = '';
 
+        // if (isset($_FILES)){
+        //     echo("<pre>");
+        //     var_dump($_FILES);
+        //     var_dump($_POST);
+        //     echo("</pre>");
+        // }
+
+        
         $html = <<<BLADE
         <article id="CreateGalery">
             <h2>Création d'une galerie</h2>
             
-            <form action="${urlActionForm}">
+            <form action="${urlActionForm}" method="POST" enctype="multipart/form-data">
                 <div class="form-group">
                     <label for="title">Titre</label>
                     <input type="text" id="title" name="title" placeholder="Mon voyage en Papouasie..." required>
@@ -32,7 +40,39 @@ class CreateGaleryView extends AppView {
                     <div class="images-list">
                         
                     </div>
+
+                    <div class="modal">
+                        <div class="card">
+                            <div class="header">
+                                <h3>Edition de l'image n°<span id="numberImage"></span></h3>
+                                <hr>
+                            </div>
+    
+                            
+                            <div class="body">
+                                <img src="">
+                                <div class="form">
+                                    <div class="form-group">
+                                        <label for="MODAL-title-image">Titre de l'image</label>
+                                        <input type="text" name="MODAL-title-image" value="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="MODAL-tag-image">Tag de l'image</label>
+                                        <input type="text" name="MODAL-tag-image" value="">
+                                    </div>
+                                </div>
+
+                                <div class="footer">
+                                    <button dataModal="valider">Valider</button>
+                                    <button dataModal="annuler">Annuler</button>
+                                </div>
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
                 </div>
+
                 <div class="form-group">
                     <label for="title">Description</label>
                     <textarea type="text" id="title" name="title" placeholder="Blablabla" required></textarea>
@@ -43,6 +83,8 @@ class CreateGaleryView extends AppView {
                     <a href="${urlRetour}">Annuler</a>
                 </div>
             </form>
+
+
             <script src="/html/js/Create-Edit_Galery.js"></script>
         </article>
         
