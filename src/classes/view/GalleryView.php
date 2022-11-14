592eff3c27ff5d0f6cfda18f85b440fe8461c719
Bradley BARBIER <bradley.barbier@outlook.fr>
2022-11-14 22:30:46 +0100

    add Pagination (#31)

--- src/classes/view/GalleryView.php
+++ src/classes/view/GalleryView.php
@@ -5,19 +5,28 @@ namespace atelier\view;
 class GalleryView extends AppView {
     public function render() : string{
         $gallery = $this->data;
-        $user = $gallery->users()->withPivot('role')->where('role', 'owner')->first();   
-        $pics = \atelier\modele\Picture::where('id_galery', '=', $this->request->get['id'])->get();
-        $number = $pics->count();
+        $number = $gallery->pictures()->count();
         $htmlPicture = "";
+
+        $page = 1;
+        $numberPicturePerPage = 22;
+        if (isset($_GET['page'])){
+            $page = $_GET['page'];
+        }
+        
+        $pics = $gallery->pictures()->skip(($page-1)*$numberPicturePerPage)->take(22)->get();
+
         foreach($pics as $p){
             $urlPic = $this->router->urlFor('picture', [['id',$p->id]]);
+            $srcImg = $p->name_file;
+            $titleImg = $p->title;
 
-            $htmlPicture .= <<<EOT
+            $htmlPicture .= <<<BLADE
             <a href="${urlPic}">
-                <img src="${p->namefile}">
-                <span>${p->title}</span>
+                <img src="${srcImg}">
+                <span>${titleImg}</span>
             </a>
-            EOT;
+            BLADE;
         }
         $tags = \atelier\modele\Tag::where('id', '=', $this->request->get['id'])->get();
         $htmlTag = '';
@@ -26,24 +35,57 @@ class GalleryView extends AppView {
             $htmlTag.= $tag->name . ', ';
         }
         
-        $html = <<<EOF
+        $titleGallery = $gallery->title;
+        $userGallery = $gallery->users()->withPivot('role')->where('role', 'owner')->first();
+        $nameUserGallery = $userGallery->username;
+        
+        $dateGallery = $gallery->created_at;
+        $dateGallery = date('d/m/Y', strtotime($dateGallery));
 
-            <div class="info">
-                <p>Titre : {$gallery->title}</p>
-                <p>Auteur : {$user->username}</p>
-                <p>Date de création : {$gallery->created_at}</p>
-            </div>
+        $idGallery = $gallery->id;
 
-            <div class="container">
-                ${htmlPicture}
-            </div>
+        $urlHere = $this->router->urlFor('view-gallery');
 
-            <div class="info-comp">
-                <p>Tags : ${htmlTag}</p>
-                <div></div>
-                <p>Nombre : ${number}</p>
+        $htmlPagination = "";
+        for ($i = 1; $i <= ceil($number/$numberPicturePerPage); $i++) {
+            if (isset($_GET['page'])){
+                $htmlPagination .= <<<BLADE
+                <option value="${i}" selected>${i}</option>
+                BLADE;
+            } else {
+                $htmlPagination .= <<<BLADE
+                <option value="${i}">${i}</option>
+                BLADE;
+            }
+        }
+        
+        $html = <<<BLADE
+        <div class="info">
+            <p>Titre : ${titleGallery}</p>
+            <p>Auteur : ${nameUserGallery}</p>
+            <p>Date de création : ${dateGallery}</p>
+            <p>Tags : ${htmlTag}</p>
+            <p>Nombre d'images : ${number}</p>
+        </div>
+
+        <div class="container">
+            ${htmlPicture}
+        </div>
+
+        <div class="info-comp">
+            
+            <div>
+                <form action="${urlHere}" method="get">
+                    <input type="hidden" name="action" value="view-gallery">
+                    <input type="hidden" name="id" value="${idGallery}">
+                    <select name="page" onChange="this.form.submit()">
+                        ${htmlPagination}
+                    </select>
+                </form>
             </div>
-        EOF;
+            
+        </div>
+        BLADE;
 
         return $html;
     }
