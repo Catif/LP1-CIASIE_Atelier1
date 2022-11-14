99290f204d4c90bfcde1ed818ba88a066007d26b
Bradley BARBIER <bradley.barbier@outlook.fr>
2022-11-14 17:46:24 +0100

    backup

--- src/classes/view/ProfileView.php
+++ src/classes/view/ProfileView.php
@@ -4,9 +4,81 @@ namespace atelier\view;
 
 class ProfileView extends AppView {
     public function render() : string{
+        $user = $this->data['user'];
+        
+
+        $myGaleries = $this->createMyGaleries();
+        $myGroups = $this->createMyGroups();
+        $sharedWithMe = $this->createSharedGaleries();
+
         $html = <<<BLADE
         <h2>Mes galeries</h2>
-        <div id="galery-list">
+        ${myGaleries}
+
+        <h2>Mes groupes</h2>
+        ${myGroups}
+
+        <h2>Partagées avec moi</h2>
+        ${sharedWithMe}
+
+        BLADE;
+
+        return $html;
+    }
+
+    
+    
+    private function createMyGaleries(){
+        $galeries = $this->data['galeries']->get();
+
+        $htmlGaleries = "";
+        foreach($galeries as $galery){
+            $titleGalery = $galery->title;
+            $pictures = $galery->pictures;
+            $numberPictures = count($pictures);
+            $srcPicture = $pictures[random_int(0, $numberPictures)]->name_file;
+
+            $htmlGaleries .= <<<BLADE
+            <div class="galery">
+                <img src='${srcPicture}'>
+            </div>
+            BLADE;
+        }
+        $html = <<<BLADE
+        <div class="list-galery">
+            ${htmlGaleries}
+        </div>
+        BLADE;
+        
+        return $html;
+    }
+
+
+
+
+
+    private function createMyGroups(){
+
+        $html = <<<BLADE
+
+
+        BLADE;
+        
+        return $html;
+    }
+
+
+
+    private function createSharedGaleries(){
+
+        $html = <<<BLADE
+        <div class="galery">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
             <div></div>
             <div></div>
             <div></div>
@@ -15,8 +87,9 @@ class ProfileView extends AppView {
             <div></div>
             <div></div>
         </div>
-        BLADE;
 
+        BLADE;
+        
         return $html;
     }
 }
