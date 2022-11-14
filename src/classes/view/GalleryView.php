1765920a51d0afa32dd596a585aa49903fdedbf4
Mehdi <mehdi_tahri@outlook.fr>
2022-11-14 20:19:54 +0100

    GALLERY VIEW

--- src/classes/view/GalleryView.php
+++ src/classes/view/GalleryView.php
@@ -8,44 +8,50 @@ class GalleryView extends AppView {
         $user = $gallery->users()->withPivot('role')->where('role', 'owner')->first();   
         $pics = \atelier\modele\Picture::where('id_galery', '=', $this->request->get['id'])->get();
         $number = $pics->count();
+        $htmlPicture = "";
+        foreach($pics as $p){
+            $urlPic = $this->router->urlFor('picture', [['id',$p->id]]);
+
+            $htmlPicture .= <<<EOT
+            <a href="${urlPic}">
+                <img src="${p->namefile}">
+                <span>${p->title}</span>
+            </a>
+            EOT;
+        }
+        $tags = \atelier\modele\Tag::where('id', '=', $this->request->get['id'])->get();
+        $htmlTag = '';
+
+
+
+        /*
+        foreach($pics as $p){
+            $r = new \atelier\router\Router();
+            $url_pic = $r->urlFor();
+        }
+        */
+
+        foreach($tags as $tag){
+            $htmlTag.= $tag->name . ', ';
+        }
         
-        
+        var_dump($tags);
         $html = <<<EOF
 
             <div class="info">
-                <p>Titre :{$gallery->title}</p>
-                <p>Auteur :{$user->username}</p>
-                <p>Date de création :{$gallery->created_at}</p>
+                <p>Titre : {$gallery->title}</p>
+                <p>Auteur : {$user->username}</p>
+                <p>Date de création : {$gallery->created_at}</p>
             </div>
 
             <div class="container">
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
-                <div>6</div>
-                <div>7</div>
-                <div>8</div>
-                <div>10</div>
-                <div>11</div>
-                <div>12</div>
-                <div>13</div>
-                <div>14</div>
-                <div>15</div>
-                <div>16</div>
-                <div>17</div>
-                <div>18</div>
-                <div>19</div>
-                <div>20</div>
-                <div>21</div>
-                <div>22</div>
+
             </div>
 
             <div class="info-comp">
-                <p>Tags :{$gallery->tags}</p>
+                <p>Tags : ${htmlTag}</p>
                 <div></div>
-                <p>Nombre :{$number}</p>
+                <p>Nombre : ${number}</p>
             </div>
         EOF;
 
