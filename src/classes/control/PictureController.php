080a2bf871ef160f48d229a5cb8fea9016d883f4
Bradley BARBIER <bradley.barbier@outlook.fr>
2022-11-14 20:53:33 +0100

    Update demo_database.sql

--- src/classes/control/PictureController.php
+++ src/classes/control/PictureController.php
@@ -7,7 +7,23 @@ use atelier\view as View;
 class PictureController extends AbstractController{
     public function execute() : void {
         View\AppView::setAppTitle("Image - PhotoMedia");
-        
+        if (isset($_GET['id'])){
+            $picture = \atelier\modele\Picture::find($_GET['id']);
+            if ($picture){
+                $view = new View\PictureView($picture);
+                $view->render();
+            } else {
+                $urlHome = $this->router->getRouteUrl('home');
+                header('Location: ' . $urlHome);
+                die();
+            }
+        } else {
+            $urlHome = $this->router->getRouteUrl('home');
+            header('Location: ' . $urlHome);
+            die();
+        }
+
+
         $vue = new View\PictureView();
         $vue->makePage();
     }
