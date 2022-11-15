8f5938411bafb80c235da16e33e39d3a73917bb4
Christopher JUE <78968379+JUEChristopher@users.noreply.github.com>
2022-11-15 01:06:24 +0100

    galeryView finished + fix others UI nightmares (#39)

--- src/classes/control/PictureController.php
+++ src/classes/control/PictureController.php
@@ -1,22 +1,26 @@
 <?php
 
 namespace atelier\control;
+use atelier\router\Router;
 use atelier\view as View;
 
 class PictureController extends AbstractController{
     public function execute() : void {
         View\AppView::setAppTitle("Image - PhotoMedia");
+
+        $router = new Router();
+
         if (isset($_GET['id'])){
             if (is_numeric($_GET['id'])){
                 $view = new View\PictureView();
                 $view->render();
             } else {
-                $urlHome = $this->router->getRouteUrl('home');
+                $urlHome = $router->urlFor('home');
                 header('Location: ' . $urlHome);
                 die();
             }
         } else {
-            $urlHome = $this->router->getRouteUrl('home');
+            $urlHome = $router->urlFor('home');
             header('Location: ' . $urlHome);
             die();
         }
